1265f0198bb2ca5fe00e313612c337da0d18aea0
PLUM IS HAPPY <32700040+poeyjeong@users.noreply.github.com>
2024-09-05 22:39:27 +0700

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,24 @@
+import 'package:startcode/screens/login.dart';
+import 'screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Users CRUD',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const Home(),
+        '/login': (context) => const Login(),
+      },
+    );
+  }
+}
