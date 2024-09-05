796ce6efcda7e1a67d7da258e1eb6437f95b7fe4
PLUM IS HAPPY <32700040+poeyjeong@users.noreply.github.com>
2024-09-05 22:38:10 +0700

    Create sidemenu.dart

---
+++ screens/sidemenu.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:startcode/models/config.dart';
+import 'package:startcode/models/users.dart';
+import 'package:startcode/screens/home.dart';
+import 'package:startcode/screens/login.dart';
+
+class SideMenu extends StatelessWidget {
+  const SideMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    String accountName = "N/A";
+    String accountEmail = "N/A";
+    String accountUrl =
+        "https://cdn-icons-png.flaticon.com/512/146/146025.png";
+
+    Users user = Configure.login;
+    print(user.toJson().toString());
+    if (user.id != null) {
+      accountName = user.fullname!;
+      accountEmail = user.email!;
+    }
+    return Drawer(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: Text(accountName),
+            accountEmail: Text(accountEmail),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(accountUrl),
+              backgroundColor: Colors.white,
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text("Home"),
+            onTap: () {
+              Navigator.pushNamed(context, Home.routeName);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.lock),
+            title: Text("Login"),
+            onTap: () {
+              Navigator.pushNamed(context, Login.routeName);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
